4ea18329897f168834098285fcbf3a9bc6474bf7
nicolaspant <96146388+nicolaspant@users.noreply.github.com>
2022-04-21 12:49:12 -0400

    added second counter

--- src/counter.js
+++ src/counter.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react'
 
 function UseStateCounter() {
   const [value, setValue] = useState(0)
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
   return (
     <React.Fragment>
       <section style={{ margin: '4rem 0' }}>
@@ -15,6 +23,14 @@ function UseStateCounter() {
         </button>
         <button className='btn' onClick={() => setValue(value + 1)}>
           Increase
+        </button>
+      </section>
+
+      <section style={{ margin: '4rem 0' }}>
+        <h2>Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          Increase Later
         </button>
       </section>
     </React.Fragment>
